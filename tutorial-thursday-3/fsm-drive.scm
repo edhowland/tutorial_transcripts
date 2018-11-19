24211c158e0c92aa89c09c6f0d4819e0ede1a820
Ed Howland <ed.howland@gmail.com>
2018-11-19 15:59:16 -0500

    added fsm-drive.scm

---
+++ tutorial-thursday-3/fsm-drive.scm
@@ -0,0 +1,14 @@
+;;; fsm-drive.scm - moves recursion from helper fsm-aux to driver: fsm
+(load "fsm-aux.scm")
+
+(define fsm-drv (lambda (lst S)
+
+  (let* ((Q0 'S0) (Q Q0) (R (fsm-aux lst S)))
+      (cond
+        [(eq? R 'ACCEPT) 'ACCEPT]
+        [(eq? R 'REJECT) 'REJECT]
+        [else (fsm-drv (cdr lst) R)]
+      ))
+))
+
+
