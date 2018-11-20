68c78b0f28886f72140ab8abb2059044ece224cc
Ed Howland <ed.howland@gmail.com>
2018-11-19 20:17:43 -0500

    checkpt: expanded nested if clauses within fsm-aux in fsm2.scm

--- tutorial-thursday-3/fsm2.scm
+++ tutorial-thursday-3/fsm2.scm
@@ -1,7 +1,7 @@
 (define fsm-aux (lambda (str state)
-  (if (null? str) 'reject
-    (let ([d (cdr str)])
-      (pmatch (list state (car str))
+  (if (null? str) (if (eq? state 'S0) 'accept 'reject)
+    (let ([bit (car str)] [d (cdr str)])
+      (pmatch (list state bit)
       [(S0 0) (fsm-aux d 'S0)]
       )
   )
