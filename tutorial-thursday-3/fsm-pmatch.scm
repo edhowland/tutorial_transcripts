7a9bc021ecf5a2830776ea1e029dca60cd786473
Ed Howland <ed.howland@gmail.com>
2018-11-20 19:12:26 -0500

    checkpt: fsm-pmatch.scm

--- tutorial-thursday-3/fsm-pmatch.scm
+++ tutorial-thursday-3/fsm-pmatch.scm
@@ -3,8 +3,17 @@
 (define fsm (lambda (str)
   (define fsm-aux (lambda (str S)
     (let ((bit (car str)))
-      (pmatch '(S bit)
-        ['(S0 0) 'S0]
+      (pmatch (list S bit)
+    ;;; Row S0
+      [(S0 0) 'S0]
+    [(S0 1) 'S1]
+      ;; Row S1
+    [(S1 0) 'S2]
+    [(S1 1) 'S0]
+    ;;; Row S2
+    [(S2 0) 'S1]
+    [(S2 1) 'S2]
+    ;;; Else reject
         [else 'REJECT]
       )
     )
