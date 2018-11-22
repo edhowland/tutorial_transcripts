dea01ccc4c9f184aec8b9d95d435ced9f8f29b03
Ed Howland <ed.howland@gmail.com>
2018-11-22 13:02:47 -0500

    checkpt: fsm-ho2.scm

--- tutorial-thursday-3/fsm-ho2.scm
+++ tutorial-thursday-3/fsm-ho2.scm
@@ -3,14 +3,20 @@
   [S0 (lambda (str) 
   (if  (null? str) 'accecpt
       (let ([bit (car str)] [d (cdr str)])
-      d
+      (case bit
+  [0 (S0 d)]
+  [1 (S1 d)]
+  )
   )
     )
 )]
   [S1 (lambda (str) 
-  (if  (null? str) 'accecpt
+  (if  (null? str) 'reject
       (let ([bit (car str)] [d (cdr str)])
-      d
+      (case bit
+        [0 (S2 d)]
+        [1 (S0 d)]
+  )
   )
     )
 )]
