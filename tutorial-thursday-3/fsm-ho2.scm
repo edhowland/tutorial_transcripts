1edfa6c001777c233d82e7e51a08853946465cbd
Ed Howland <ed.howland@gmail.com>
2018-11-22 12:57:52 -0500

    checkpt: added lambdas into letrec in fsm-ho2.scm

--- tutorial-thursday-3/fsm-ho2.scm
+++ tutorial-thursday-3/fsm-ho2.scm
@@ -1,8 +1,26 @@
 (define fsm-ho (lambda (str)
   (letrec (
-  [S0 (lambda (str) str)]
-  [S1 (lambda (str) str)]
-  [S2 (lambda (str) str)]
+  [S0 (lambda (str) 
+  (if  (null? str) 'accecpt
+      (let ([bit (car str)] [d (cdr str)])
+      d
+  )
+    )
+)]
+  [S1 (lambda (str) 
+  (if  (null? str) 'accecpt
+      (let ([bit (car str)] [d (cdr str)])
+      d
+  )
+    )
+)]
+  [S2 (lambda (str) 
+  (if  (null? str) 'accecpt
+      (let ([bit (car str)] [d (cdr str)])
+      d
+  )
+    )
+)]
   )
   (S0 str)
   )
