6463ff223411320f302a2a4ec088a15a66f5c732
Ed Howland <ed.howland@gmail.com>
2018-11-23 13:16:01 -0500

    partial version of fsm-bit.scm

---
+++ tutorial-thursday-3/fsm-bit.scm
@@ -0,0 +1,18 @@
+;;; fsm-bit.scm
+(define fsm-bit (lambda (str)
+;;  (define fsm-aux (lambda (state bit)
+
+;;  ))
+
+  (letrec (
+  [S0 (lambda (bit) bit)]
+  [S1 (lambda (bit) bit)]
+  [S2 (lambda (bit) bit)]
+
+
+
+  )
+  S0
+  )
+  0
+))
