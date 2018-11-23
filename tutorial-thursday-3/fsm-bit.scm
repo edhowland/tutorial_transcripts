520e03f08df2e32cfac6c90129de495fe20417d4
Ed Howland <ed.howland@gmail.com>
2018-11-23 13:53:46 -0500

    checkpt: working S0,0 state transition for fsm-bit.scm

--- tutorial-thursday-3/fsm-bit.scm
+++ tutorial-thursday-3/fsm-bit.scm
@@ -1,12 +1,17 @@
 ;;; fsm-bit.scm
 (define fsm-bit (lambda (str)
   (define fsm-aux (lambda (state str)
-    (if (null? str) #t
+    (if (null? str) (state 'end)
   (fsm-aux (state (car str)) (cdr str)))
  ))
 
   (fsm-aux (letrec (
-  [S0 (lambda (bit) bit)]
+  [S0 (lambda (bit) 
+      (case bit
+      [0 S0]
+      [1 S1]
+  [else 'accept])
+)]
   [S1 (lambda (bit) bit)]
   [S2 (lambda (bit) bit)]
 
